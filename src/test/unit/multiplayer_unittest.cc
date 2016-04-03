58fc87f499ba94b49d86485c8432a2a9bf9c2df6
Echelon9 <rhyskidd@gmail.com>
2016-04-03 13:42:39 -0400

    Add unit test infrastructure and first test
    Provide Google Test-based unit test infrastructure, which is run
    automatically by Travis-CI for each row in the build matrix.
    
    Can be manually run with 'make test' from top level.
    
    Initial unit test implemented for src/multiplayer.cpp

---
+++ src/test/unit/multiplayer_unittest.cc
@@ -0,0 +1,47 @@
+/*
+    Copyright 2016 Rhys Kidd
+
+    This file is part of The Soul Ride Engine, see http://soulride.com
+
+    The Soul Ride Engine is free software; you can redistribute it
+    and/or modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2 of
+    the License, or (at your option) any later version.
+
+    The Soul Ride Engine is distributed in the hope that it will be
+    useful, but WITHOUT ANY WARRANTY; without even the implied
+    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+    See the GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#include "gtest/gtest.h"
+
+#include "multiplayer.hpp"
+
+
+TEST(MultiplayerTest, NumberOfLocalPlayers)
+{
+    // Test equality
+    MultiPlayer::SetNumberOfLocalPlayers(2);
+    EXPECT_EQ(2, MultiPlayer::NumberOfLocalPlayers());
+
+    // Test inequality
+    MultiPlayer::SetNumberOfLocalPlayers(2);
+    EXPECT_NE(1, MultiPlayer::NumberOfLocalPlayers());
+}
+
+TEST(MultiplayerTest, CurrentPlayerIndex)
+{
+    // Test equality
+    MultiPlayer::SetCurrentPlayerIndex(2);
+    EXPECT_EQ(2, MultiPlayer::CurrentPlayerIndex());
+
+    // Test inequality
+    MultiPlayer::SetCurrentPlayerIndex(2);
+    EXPECT_NE(1, MultiPlayer::CurrentPlayerIndex());
+}
+

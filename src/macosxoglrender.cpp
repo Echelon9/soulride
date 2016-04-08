f811a602f03f7de4ef99135c6d1e23b1b8710cf6
Echelon9 <rhyskidd@gmail.com>
2016-04-07 20:34:37 -0400

    const char parameter to CheckOGLError()
    Avoids GCC and Clang warnings:
    
    oglrender.cpp: In function ‘void Render::OpenOGL()’:
    oglrender.cpp:836:27: warning: deprecated conversion from string constant to ‘char*’ [-Wwrite-strings]
    
    oglrender.cpp: In function ‘void Render::ShowFrame()’:
    oglrender.cpp:883:29: warning: deprecated conversion from string constant to ‘char*’ [-Wwrite-strings]
    
    oglrender.cpp: In function ‘void Render::ClearFrame()’:
    oglrender.cpp:895:30: warning: deprecated conversion from string constant to ‘char*’ [-Wwrite-strings]
    
    Whilst here, improve documentation of CheckOGLError().

--- src/macosxoglrender.cpp
+++ src/macosxoglrender.cpp
@@ -339,7 +339,7 @@ const int	DesiredColorDepth = 16;
 
   // windows specific code removed too
 
-void	CheckOGLError(char* FuncName);
+void	CheckOGLError(const char* FuncName);
 
 
 HGLRC	RenderingContext = 0;
@@ -1246,9 +1246,16 @@ void	WriteScreenshotFilePPM(const char* filename)
 }
 
 
-void	CheckOGLError(char* FuncName)
-// Checks for OpenGL error codes.  If there's an error, throws an exception
-// with an error message which includes the given function name.
+/**
+ * Checks for OpenGL error codes.
+ *
+ * If there's an error, throws an exception with an error message which includes
+ * the given function name.
+ *
+ * @param FuncName   Provide callee function for logging purposes
+ */
+void
+CheckOGLError(const char* FuncName)
 {
 	GLenum	error = glGetError();
 	if (error == GL_NO_ERROR) return;
